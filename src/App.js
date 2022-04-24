8f8e482d8ef1febd54bd441f5a19465b7a890004
Yuji_Itadorii <rawat141abhay@gmail.com>
2022-04-24 16:31:42 +0530

    About adding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import About from './component/About';
 import Navbar from './component/Navbar';
-import Textform from './component/Textform';
+// import Textform from './component/Textform';
 function App() {
   return (
     <>
     <Navbar title="TextEditor" />
     <div className="container my-5">
-    <Textform heading="Editor"/>
+    {/* <Textform heading="Editor"/> */}
+    <About/>
     </div>
     </>
   );
--- src/component/Textform.js
+++ src/component/Textform.js
@@ -3,24 +3,43 @@ import PropTypes from 'prop-types'
 
 
 export default function Textform(props) {
-    const [text,setText] = useState();
+    const [text,setText] = useState("");
     const handleOnclick = ()=>{
         let newText = text.toUpperCase(); 
         setText(newText);
+    }
+    
+    const convertLower = ()=>{
+        let newtext = text.toLowerCase();
+        setText(newtext);
+    }
+    const clearText = ()=>{
+        setText("");
     }
 
     const handleOnchange = (event)=>{
         setText(event.target.value);
     }
+
     return (
-        <div>
+        <>
+        <div className='conatiner'>
             <h2>{props.heading}</h2>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">Text area</label>
                 <textarea className="form-control" onChange={handleOnchange} value={text} id="box" rows="8"></textarea>
             </div>
+            <div className="container">
             <button className="btn btn-primary" onClick={handleOnclick} >Convert to uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={convertLower} >Convert to lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={clearText} >Clear text</button>
+            </div>
         </div>
+        <div className="container mt-3">
+            <h4>Your text summary</h4>
+            <p> {text.split(" ").length-1} Words and {text.length} Characters </p>
+        </div>
+        </>
     )
 }
 
